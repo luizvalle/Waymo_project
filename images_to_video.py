23e9daccae1ef73571af034060ad17de9386b20c
fyj97 <53674931+fyj97@users.noreply.github.com>
2020-06-19 16:54:59 +0800

    Add files via upload

---
+++ images_to_video.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+
+fps = 10
+size = (1920, 1080)
+videowriter = cv2.VideoWriter("test.mp4", cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, size)
+path = '/media/yongjie/Seagate Expansion Drive/Waymo_test_coordinate/images/'
+pathlist = os.listdir(path)
+print(len(pathlist))
+pathlist.sort()
+j=1
+for i in pathlist:
+    img = cv2.imread(path + i)
+	print(j)
+    j=j+1
+    videowriter.write(img)
+videowriter.release()
+cv2.destroyAllWindows()
